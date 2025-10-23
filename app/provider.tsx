1914cb2711da1b87cc19e24ae72744266672de40
pavanctruh <pavan@ctruh.com>
2025-10-23 19:24:47 +0530

    working on ui

---
+++ app/provider.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./_components/Header";
+
+function provider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <Header />
+
+      {children}
+    </div>
+  );
+}
+
+export default provider;
