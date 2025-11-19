c2c016d128e12606247d6b6591df751503e5cbaf
pavanctruh <pavan@ctruh.com>
2025-11-19 18:24:17 +0530

    updated frontend and backend

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,34 +1,15 @@
-<<<<<<< HEAD
-// app/provider.tsx
 "use client";
 
+import { ClerkProvider } from "@clerk/nextjs";
 import Header from "./_components/Header";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen flex flex-col">
-      <Header />
-      <main className="flex-1 overflow-hidden">{children}</main>
-    </div>
+    <ClerkProvider>
+      <div className="h-screen flex flex-col">
+        <Header />
+        <main className="flex-1 overflow-hidden">{children}</main>
+      </div>
+    </ClerkProvider>
   );
 }
-=======
-import React from "react";
-import Header from "./_components/Header";
-
-function provider({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <div>
-      <Header />
-
-      {children}
-    </div>
-  );
-}
-
-export default provider;
->>>>>>> 1914cb2711da1b87cc19e24ae72744266672de40
--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
+        hostname: "images.unsplash.com"
       },
       {
         protocol: "https",
-        hostname: "plus.unsplash.com",
-      },
-    ],
-  },
-=======
-  /* config options here */
->>>>>>> 1914cb2711da1b87cc19e24ae72744266672de40
+        hostname: "plus.unsplash.com"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
